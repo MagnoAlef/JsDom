b98ae3ccf7dd0b1c39839598682296a29c8bc1cf
Magno <magno.alefb@gmail.com>
2024-01-02 22:32:01 -0300

    teste com valores do dom

--- EnviandoValoresDom/index.js
+++ EnviandoValoresDom/index.js
@@ -1,12 +1,28 @@
 function register(element){
 const username = element.children.username.value
 const password = element.children.password.value
-const passwordConfirmation = element.passwordConfirmation.value
+const passwordConfirmation = element.children.passwordConfirmation.value
+console.log({username,password,passwordConfirmation})
 
 if(password === passwordConfirmation){
-    alert('Usuario' + username + 'Registrado')
+    alert('Usuario ' + username + ' Registrado')
+    
+    const registerUsername = document.getElementById('register')
+    
+    const newLi = document.createElement('li')
+    newLi.innerText = `${username}`
+    
+    registerUsername.appendChild(newLi)
+
+    element.children.username.value = ''
+    element.children.password.value = ''
+    element.children.passwordConfirmation.value = ''
 }else{
     alert('As senhas não conferem')
+    element.children.username.value = ''
+    element.children.password.value = ''
+    element.children.passwordConfirmation.value = ''
 }
     console.log(element) // Saida a <section> completa
+    
 }
