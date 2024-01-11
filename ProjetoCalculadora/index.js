e129485c80dbe5f18772ef15618ae5b6eeab17be
Magno <magno.alefb@gmail.com>
2024-01-11 18:01:46 -0300

    finalizando exercicio

--- ProjetoCalculadora/index.js
+++ ProjetoCalculadora/index.js
@@ -5,24 +5,24 @@ const resultInput = document.getElementById("result")
 const allowedKeys = ["(", ")", "/", "*", "-", "+", "9", "8", "7", "6", "5", "4", "3", "2", "1", "0", ".", "%", " "]
 
 document.querySelectorAll(".charKey").forEach(function (charKeyBtn) {
-  charKeyBtn.addEventListener("click", function () {
-    const value = charKeyBtn.dataset.value
+  charKeyBtn.addEventListener("click", function () { // Adicionando uma evento para cada botao clicado
+    const value = charKeyBtn.dataset.value // Dataset propriedade para acessa os ateributos  data
     input.value += value
   })
 })
 
 document.getElementById("clear").addEventListener("click", function () {
   input.value = ""
-  input.focus()
+  input.focus() // Da um focus na tela quando limpar
 })
 
-input.addEventListener("keydown", function (ev) {
+input.addEventListener("keydown", function (ev) { // Keydown quando uma tecla e precionada
   ev.preventDefault()
-  if (allowedKeys.includes(ev.key)) {
+  if (allowedKeys.includes(ev.key)) { // Verificação somente para permitir numeros validos
     input.value += ev.key
     return
   }
-  if (ev.key === "Backspace") {
+  if (ev.key === "Backspace") { // Usar o Backspace para apagar 
     input.value = input.value.slice(0, -1)
   }
   if (ev.key === "Enter") {
@@ -35,24 +35,24 @@ document.getElementById("equal").addEventListener("click", calculate)
 function calculate() {
   resultInput.value = "ERROR"
   resultInput.classList.add("error")
-  const result = eval(input.value)
+  const result = eval(input.value)// eval serve para avaliar o codigo js -> como estou passando o valor pelo input ele vai calcular
   resultInput.value = result
   resultInput.classList.remove("error")
 }
 
 document.getElementById("copyToClipboard").addEventListener("click", function (ev) {
-  const button = ev.currentTarget
+  const button = ev.currentTarget // Quem acionou o evento
   if (button.innerText === "Copy") {
     button.innerText = "Copied!"
     button.classList.add("success")
-    navigator.clipboard.writeText(resultInput.value)
+    navigator.clipboard.writeText(resultInput.value) // Copiar texto para area de transferencia
   } else {
     button.innerText = "Copy"
     button.classList.remove("success")
   }
 })
 
-document.getElementById("themeSwitcher").addEventListener("click", function () {
+document.getElementById("themeSwitcher").addEventListener("click", function () { // Troca de dar para ligth
   if (main.dataset.theme === "dark") {
     root.style.setProperty("--bg-color", "#f1f5f9")
     root.style.setProperty("--border-color", "#aaa")
@@ -61,7 +61,7 @@ document.getElementById("themeSwitcher").addEventListener("click", function () {
     main.dataset.theme = "light"
   } else {
     root.style.setProperty("--bg-color", "#212529")
-    root.style.setProperty("--border-color", "#666")
+    root.style.setProperty("--border-color", "#555")
     root.style.setProperty("--font-color", "#f1f5f9")
     root.style.setProperty("--primary-color", "#4dff91")
     main.dataset.theme = "dark"
